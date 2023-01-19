c4dfb2c0c759cee59b0761a0eb0500555a82a048
PerdiDev <perdidev@gmail.com>
2023-01-18 17:17:53 -0800

    Menyelesaikan halaman index.html

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2.5rem',
+        lg: '5.5rem',
+        xl: '6rem',
+        '2xl': '7rem',
+      },
+    },
+    fontFamily: {
+      'sans': 'Jaldi',
+    },
+    extend: {
+      colors: {
+        'green': '#00681D',
+        'blue': '#091685',
+        'white-1': '#F1E9E9',
+        'blue-one': '#06053A',
+        'blue-two': '#0E1020',
+        'detail': '#06053A',
+        'green-button': '#09D442',
+        'blue-button': '#000D80',
+      },
+    },
+  },
+  plugins: [],
+}
